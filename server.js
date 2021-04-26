de954383563c616a91f31d22e06c448d25128e17
Kuleksoft <kuleksoft@gmail.com>
2021-04-26 07:11:21 +0200

    deploy 2

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
+app.use(express.static(__dirname + '/dist/playlist-voter'));
 
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/playlist-voter/index.html'));
 });
 
 
