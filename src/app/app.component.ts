cdca92c3c903200de28f5d88b8b0b036c3499c27
Kuleksoft <kuleksoft@gmail.com>
2021-04-26 05:41:36 +0200

    work work work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgxCsvParser } from 'ngx-csv-parser';
 
 @Component({
   selector: 'pv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'playlist-voter';
+export class AppComponent implements OnInit {
+
+  public currentIndex: number = 0;
+  public data: any[];
+  public answers: string;
+
+  constructor(private readonly http: HttpClient, private ngxCsvParser: NgxCsvParser) {}
+
+  public ngOnInit(): void {
+    this.getData('lata5060');
+  }
+
+  public getData(fileName: string): void {
+
+    this.currentIndex = 0;
+    this.data = null;
+    this.answers = '';
+
+    this.http.get(`assets/${fileName}.csv`, { responseType: 'text' }).subscribe((response: string) => {
+      const rawData = this.ngxCsvParser.csvStringToArray(response,',');
+      const header = this.ngxCsvParser.getHeaderArray(rawData);
+      const data = this.ngxCsvParser.getDataRecordsArrayFromCSVFile(rawData, 4, { header: true });
+      const parsedData = [];
+      data.map((element: Array<string>) => {
+        const entry = {};
+        header.forEach((property: string, index: number) => {
+          entry[property] = element[index];
+        })
+        parsedData.push(entry);
+      });
+      parsedData.forEach(el => el.score = 0);
+      this.data = parsedData;
+      this.answers = this.data.map(el => el.score).join('\n');
+    });
+  }
+
+  public getAnswers(): string {
+    return this.answers;
+  }
+
+  public setIndex(index: number) {
+    this.currentIndex = index;
+  }
+
+  public setScore(value: number): void {
+    this.data[this.currentIndex].score = value;
+    this.answers = this.data.map(el => el.score).join('\n');
+    if (this.currentIndex !== this.data.length - 1) {
+      this.currentIndex++;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgxCsvParserModule } from 'ngx-csv-parser';
+
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxCsvParserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatMenuModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
